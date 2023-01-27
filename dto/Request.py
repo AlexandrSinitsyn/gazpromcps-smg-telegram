998f27f028a70acf31b4f6a6cdb1b93d34d727d8
na3t1e <na3t1e@gmail.com>
2023-01-27 12:40:23 +0300

    .

---
+++ dto/Request.py
@@ -0,0 +1,6 @@
+class Request:
+    sender = None  # type: User
+    section_number = -1
+    message = ''
+    mesurnment = ''
+    count = -1
---
+++ dto/Response.py
@@ -0,0 +1,3 @@
+class Response:
+    addresser = None  # type: User
+    csv = ''  # path
