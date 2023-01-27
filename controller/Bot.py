ce7be8d8b734534eb335b7704a3b458b370e6421
AlexSin <Sannitsa@yahoo.com>
2023-01-27 23:24:17 +0300

    fix: imports

--- controller/Bot.py
+++ controller/Bot.py
@@ -1,4 +1,6 @@
-from service import *
+from service.UserService import UserService
+from service.JobService import JobService
+from service.ExcelService import ExcelService
 
 user_service = UserService()
 job_service = JobService()
